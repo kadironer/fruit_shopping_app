a3dd4d7127e275502bd58f1620fb68e51acd6c92
Kadir Oner <kadironer46500@gmail.com>
2022-07-12 01:26:41 +0300

    Login Screen Completed

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fruit Shopping',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -43,7 +44,7 @@ class _HomePageState extends State<HomePage> {
             Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
-              color: Color(0xFF2C2C2C),
+              color: const Color(0xFF2C2C2C),
             ),
             Center(
               child: Column(
@@ -53,8 +54,8 @@ class _HomePageState extends State<HomePage> {
                       width: 300,
                       child: Image.asset("images/vegetablesFruit.png"),
                   ),
-                  SizedBox(
-                    height: 150,
+                  const SizedBox(
+                    height: 350,
                   )
                 ],
               ),
@@ -62,20 +63,108 @@ class _HomePageState extends State<HomePage> {
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+                children: const [
                   SizedBox(
-                    height: 125,
+                    height: 50,
                   ),
                   Text("KEEPING YOU HEALTHY",
                     style: TextStyle(
+                      fontSize: 12.0,
                       fontFamily: "Inter",
-                      fontWeight: FontWeight.w700,
+                      fontWeight: FontWeight.normal,
                       color: Color(0xFFF1C950),
                       letterSpacing: 7.0,
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text("Let's start eating\nHealthy",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Inter",
+                    fontSize: 40.0,
+                    letterSpacing: 2.0,
+                  ),
+                  ),
 
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text("In honor of World Health Day\n"
+                      "we'd like to give you this\n"
+                      "amazing offer.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white30,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Inter",
+                        fontSize: 15.0,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
                 ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 90.0),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: TextButton(
+                  child: Text("Already have an Account? Sign-in",
+                  style:TextStyle(
+                    color: Color(0xFFF1C950),
+                    fontSize: 12.0,
+                    fontFamily: "Inter",
+                    fontWeight: FontWeight.w700
+                  ),
+                  ),
+                  onPressed: (){
+
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: 320,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFFF1C950),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      )
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text("Get Started",
+                        style: TextStyle(
+                          color: Color(0xFF2C2C2C),
+                          fontSize: 17.0,
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w700,
+                        ),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon( // <-- Icon
+                          Icons.arrow_right_alt,
+                          size: 24.0,
+                          color: Color(0xFF2C2C2C),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
