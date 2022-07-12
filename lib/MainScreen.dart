45e48c7d7cc4a6a98d684d4ed148022f48fd6ea0
Kadir Oner <kadironer46500@gmail.com>
2022-07-12 20:26:55 +0300

    Fruit Category completed

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:proje_fruit_shopping/allFruits.dart';
+import 'package:proje_fruit_shopping/fruits.dart';
+
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -9,6 +11,35 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  Future<List<Fruits>> getFruits() async{
+    var fruitList=<Fruits>[];
+
+    var f1=Fruits(1,"Fruit", "Apple", "images/fruits/apple.png", 15.00);
+    var f2=Fruits(2,"Fruit", "Banana", "images/fruits/banana.png", 23.00);
+    var f3=Fruits(3,"Fruit", "BlueBerry", "images/fruits/blueberry.png", 24.99);
+    var f4=Fruits(4,"Fruit", "Cherry", "images/fruits/cherry.png", 9.99);
+    var f5=Fruits(5,"Fruit", "Grapes", "images/fruits/grapes.png", 20.00);
+    var f6=Fruits(6,"Fruit", "Kiwi", "images/fruits/kiwi.png", 44.99);
+    var f7=Fruits(7,"Fruit", "Mandarin", "images/fruits/mandarin.png", 36.50);
+    var f8=Fruits(8,"Fruit", "Orange", "images/fruits/orange.png", 30.25);
+    var f9=Fruits(9,"Fruit", "Pear", "images/fruits/pear.png", 28.75);
+    var f10=Fruits(10,"Fruit", "Strawberry", "images/fruits/strawberry.png", 43.99);
+
+    fruitList.add(f1);
+    fruitList.add(f2);
+    fruitList.add(f3);
+    fruitList.add(f4);
+    fruitList.add(f5);
+    fruitList.add(f6);
+    fruitList.add(f7);
+    fruitList.add(f8);
+    fruitList.add(f9);
+    fruitList.add(f10);
+
+    return fruitList;
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +48,7 @@ class _MainScreenState extends State<MainScreen> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            color: Color(0xFF1E1E1E),
+            color: const Color(0xFF1E1E1E),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 20.0,left: 20.0,right: 20.0),
@@ -25,7 +56,7 @@ class _MainScreenState extends State<MainScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
-                  icon: Icon(Icons.menu,size: 30.0,color: Colors.white,),
+                  icon: const Icon(Icons.menu,size: 30.0,color: Colors.white,),
                   tooltip: "Menu",
                   onPressed: (){
 
@@ -34,7 +65,7 @@ class _MainScreenState extends State<MainScreen> {
                 Row(
                   children: [
                     IconButton(
-                      icon:Icon(Icons.shopping_bag_outlined,color: Colors.white),
+                      icon:const Icon(Icons.shopping_bag_outlined,color: Colors.white),
                       tooltip: "Cart",
                       onPressed: (){
                       },
@@ -49,7 +80,7 @@ class _MainScreenState extends State<MainScreen> {
                           clipBehavior: Clip.antiAliasWithSaveLayer,
                           color: Colors.transparent,
                           child: Image.asset("images/kadirProfile.jpg",fit: BoxFit.fill,),
-                          shape: RoundedRectangleBorder(
+                          shape: const RoundedRectangleBorder(
                             borderRadius: BorderRadius.all(Radius.circular(15.0)),
                           ),
                         ),
@@ -74,7 +105,7 @@ class _MainScreenState extends State<MainScreen> {
                       semanticContainer: true,
                       clipBehavior: Clip.antiAliasWithSaveLayer,
                       color: Colors.transparent,
-                      shape: RoundedRectangleBorder(
+                      shape: const RoundedRectangleBorder(
                         borderRadius: BorderRadius.all(Radius.circular(15.0))
                       ),
                       child: Image.asset("images/campaigns.png",fit: BoxFit.fill),
@@ -84,10 +115,46 @@ class _MainScreenState extends State<MainScreen> {
               ),
             ),
           ),
-          Column(
-            children: [
-              Text("Fruits")
-            ],
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0,left: 30.0,right: 20.0),
+            child: Align(
+            alignment: Alignment.centerLeft,
+              child: Row(
+
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Fruits",
+                    style:TextStyle(
+                      color: Colors.white,
+                      fontFamily: "Inter",
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20.0
+                    ),
+                  ),
+                  TextButton(
+                    onPressed:(){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const AllFruits()));
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text("View All",
+                            style: TextStyle(
+                              color: Color(0xFFF1C950),
+                              fontFamily: "Inter",
+                              fontWeight: FontWeight.normal
+                            ),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(Icons.arrow_forward,color: Color(0xFFF1C950),),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
